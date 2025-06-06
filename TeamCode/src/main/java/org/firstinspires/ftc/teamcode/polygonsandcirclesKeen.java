b73e4f64766463b57089d5f28ac417cbe43b5bb0
kleung1 <keenleung678@gmail.com>
2025-06-06 22:49:24 +1200

    fast load implemented

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/polygonsandcirclesKeen.java
@@ -0,0 +1,164 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp(name = "polygonsandcirclesKeen", group = "Testing")
+public class polygonsandcirclesKeen extends LinearOpMode {
+    OpenCvCamera camera;
+    boolean cameraOpened = false;
+
+    @Override
+    public void runOpMode() {
+        int viewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, viewId);
+
+        ShapeDetectionPipeline pipeline = new ShapeDetectionPipeline();
+        camera.setPipeline(pipeline);
+        camera.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+
+        telemetry.addLine("Waiting for start...");
+        telemetry.update();
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                try {
+                    camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+                    cameraOpened = true;
+                } catch (Exception e) {
+                    telemetry.addLine("640x480 failed, trying lower resolution...");
+                    telemetry.update();
+                    try {
+                        camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                        cameraOpened = true;
+                    } catch (Exception fallbackError) {
+                        telemetry.addLine("Camera failed to start");
+                        telemetry.update();
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera error", errorCode);
+                telemetry.update();
+            }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (!cameraOpened) {
+                telemetry.addLine("Camera not opened");
+                telemetry.update();
+            }
+            sleep(50);
+        }
+
+        camera.stopStreaming();
+    }
+
+    static class ShapeDetectionPipeline extends OpenCvPipeline {
+        private final Mat hsv = new Mat();
+        private final Mat maskRed = new Mat();
+        private final Mat maskBlue = new Mat();
+        private final Mat output = new Mat();
+
+        private static final double CIRCULARITY_THRESHOLD = 0.75;
+        private static final double RECTANGLE_ASPECT_TOLERANCE = 0.3;
+        private static final int MIN_AREA = 500;
+
+        @Override
+        public Mat processFrame(Mat input) {
+            Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
+
+            Core.inRange(hsv, new Scalar(0, 100, 100), new Scalar(10, 255, 255), maskRed);
+            Core.inRange(hsv, new Scalar(100, 100, 100), new Scalar(130, 255, 255), maskBlue);
+
+            input.copyTo(output);
+
+            processMask(maskRed, output, new Scalar(255, 0, 0));
+            processMask(maskBlue, output, new Scalar(0, 0, 255));
+
+            maskRed.release();
+            maskBlue.release();
+            hsv.release();
+
+            return output;
+        }
+
+        private void processMask(Mat mask, Mat outputImage, Scalar color) {
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+
+            Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            for (MatOfPoint contour : contours) {
+                double area = Imgproc.contourArea(contour);
+                if (area < MIN_AREA) continue;
+
+                MatOfPoint2f curve = new MatOfPoint2f(contour.toArray());
+                MatOfPoint2f approxCurve = new MatOfPoint2f();
+                double epsilon = 0.02 * Imgproc.arcLength(curve, true);
+                Imgproc.approxPolyDP(curve, approxCurve, epsilon, true);
+
+                double perimeter = Imgproc.arcLength(curve, true);
+                double circularity = (4 * Math.PI * area) / (perimeter * perimeter);
+
+                String shapeLabel;
+                Point[] points = approxCurve.toArray();
+
+                if (circularity > CIRCULARITY_THRESHOLD) {
+                    shapeLabel = "Circle";
+                } else if (points.length == 4) {
+                    Rect rect = Imgproc.boundingRect(new MatOfPoint(points));
+                    double aspectRatio = rect.width / (double)rect.height;
+                    if (Math.abs(aspectRatio - 1.0) < RECTANGLE_ASPECT_TOLERANCE) {
+                        shapeLabel = "Square";
+                    } else {
+                        shapeLabel = "Rectangle";
+                    }
+                } else if (points.length == 3) {
+                    shapeLabel = "Triangle";
+                } else if (points.length > 4) {
+                    shapeLabel = "Polygon";
+                } else {
+                    shapeLabel = "Unknown";
+                }
+
+                Moments moments = Imgproc.moments(contour);
+                Point center = new Point(moments.m10 / moments.m00, moments.m01 / moments.m00);
+
+                Imgproc.drawContours(outputImage, List.of(new MatOfPoint(points)), -1, color, 2);
+                Imgproc.putText(outputImage, shapeLabel, center, Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(255, 255, 255), 2);
+
+                curve.release();
+                approxCurve.release();
+            }
+
+            hierarchy.release();
+        }
+    }
+}
